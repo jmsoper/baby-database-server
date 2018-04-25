6fada83b3d915b8a4266dc78eacb6f2b724e0e9f
jmsoper <jmsoper@gmail.com>
2018-04-24 23:31:47 -0400

    clean up vars

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
 
 class DataStore {
   constructor () {
@@ -39,15 +40,15 @@ const composeHTML = function(content) {
     '</html>');
 }
 
-var requestProcessor = function(req, res, datastore) {
-  var page = url.parse(req.url).pathname;
-  var params = querystring.parse(url.parse(req.url).query);
-  var starterContent = "<p>To read from the database, append <strong>?key=<em>desiredKey</em></strong> to the URL.</p><p>To write to the database, append <strong>?<em>yourKey</em>=<em>yourValue</em></strong> to the URL.</p>";
-  var responseData = { status: 200, content: starterContent }
+const requestProcessor = function(req, res, datastore) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  const starterContent = "<p>To read from the database, append <strong>?key=<em>desiredKey</em></strong> to the URL.</p><p>To write to the database, append <strong>?<em>yourKey</em>=<em>yourValue</em></strong> to the URL.</p>";
+  let responseData = { status: 200, content: starterContent }
 
   if (page === '/get'){
     if (params && params.key){
-      var key = params.key;
+      const key = params.key;
       datastore.getItem({ key: params.key, response: responseData });
     }
   } else if (page === '/set'){
@@ -65,8 +66,8 @@ var requestProcessor = function(req, res, datastore) {
   res.end();
 }
 
-var datastore = new DataStore();
+const datastore = new DataStore();
 
-var server = http.createServer((req, res) => requestProcessor(req, res, datastore));
+const server = http.createServer((req, res) => requestProcessor(req, res, datastore));
 server.listen(PORT);
 console.log("Server running on port " + String(PORT));
