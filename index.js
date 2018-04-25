6b94968ec676b8277f200dc8a250e55793105540
jmsoper <jmsoper@gmail.com>
2018-04-24 22:43:51 -0400

    little cleanup

--- index.js
+++ index.js
@@ -7,35 +7,8 @@ var database = {};
 
 var portInUse = 8080;
 
-var server = http.createServer(function(req, res) {
-  var page = url.parse(req.url).pathname;
-  var status = 200;
-  var params = querystring.parse(url.parse(req.url).query);
-  var content = "<p>To read from the database, append <strong>?key=<em>desiredKey</em></strong> to the URL.</p>";
-  content += "<p>To write to the database, append <strong>?<em>yourKey</em>=<em>yourValue</em></strong> to the URL.</p>";
-
-  if (page === '/get'){
-    if (params && params.key){
-      var key = params.key;
-      if (database[key]){
-        content += ("\n " + String(key) + ": " + database[key]);
-      } else {
-        content += ("\n The key '" + String(key) + "' does not exist in the current database.");
-      }
-    }
-  } else if (page === '/set'){
-    if (params){
-      for (key in params){
-        database[key] = params[key];
-        content += ("<p>The value '" + params[key] + "' has been stored to the database at '" + key + "'</p>");
-      }
-    }
-  } else {
-    status = 404;
-    content = "You're lost! Try going <a href='\/get'>here</a>.";
-  }
-  res.writeHead(status, {'Content-type': 'text/html'});
-  res.write('<!DOCTYPE html>'+
+var composeHTML = function(content) {
+  return ('<!DOCTYPE html>'+
     '<html>'+
     ' <head>'+
     ' <meta charset="utf-8" />'+
@@ -45,6 +18,37 @@ var server = http.createServer(function(req, res) {
     ' ' + content +
     ' </body>' +
     '</html>');
+}
+
+var server = http.createServer(function(req, res) {
+  var page = url.parse(req.url).pathname;
+  var params = querystring.parse(url.parse(req.url).query);
+  var starterContent = "<p>To read from the database, append <strong>?key=<em>desiredKey</em></strong> to the URL.</p><p>To write to the database, append <strong>?<em>yourKey</em>=<em>yourValue</em></strong> to the URL.</p>";
+  var responseData = { status: 200, content: starterContent }
+
+  if (page === '/get'){
+    if (params && params.key){
+      var key = params.key;
+      if (database[key]){
+        responseData.content += ("\n " + String(key) + ": " + database[key]);
+      } else {
+        responseData.status = 404;
+        responseData.content += ("\n The key '" + String(key) + "' does not exist in the current database.");
+      }
+    }
+  } else if (page === '/set'){
+    if (params){
+      for (key in params){
+        database[key] = params[key];
+        responseData.content += ("<p>The value '" + params[key] + "' has been stored to the database at '" + key + "'</p>");
+      }
+    }
+  } else {
+    responseData.status = 404;
+    responseData.content = "You're lost! Try going <a href='\/get'>here</a>.";
+  }
+  res.writeHead(responseData.status, {'Content-type': 'text/html'});
+  res.write(composeHTML(responseData.content));
   res.end();
 });
 
